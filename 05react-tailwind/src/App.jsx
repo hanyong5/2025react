d73bbf1b195ca35e9ef142014f938827bc386b7d
hanyong5 <hanyong5@naver.com>
2025-04-07 12:25:37 +0900

    todo

--- 05react-tailwind/src/App.jsx
+++ 05react-tailwind/src/App.jsx
@@ -1,33 +1,32 @@
 import React from "react";
+import data from "./assets/data";
 
 function App() {
   return (
     <>
-      <div className="p-4 border w-[500px] m-auto mt-12">
-        <h1 className="text-2xl font-bold mb-4 text-center">TODO app</h1>
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="할일을 입력하세요"
-            className="flex-1 border p-2 rounded border-gray-300 focus:outline-none"
-          />
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            추가
-          </button>
-        </div>
-        <ul className="py-2">
-          <li className="flex justify-between items-center py-4 border-b">
-            <span>test</span>
-            <button className="text-red-500 text-sm hover:underline">
-              삭제
-            </button>
-          </li>
-          <li className="flex justify-between items-center py-4 border-b">
-            <span className="line-through text-gray-400">test</span>
-            <button className="text-red-500 text-sm hover:underline">
-              삭제
-            </button>
-          </li>
+      <div className="container m-auto">
+        <ul className="flex flex-row flex-wrap sm:flex-row sm:flex-wrap gap-2">
+          {data.map((item, i) => {
+            return (
+              <>
+                <li className="flex-1 min-w-[45%] sm:min-w-[30%] ">
+                  <div className="w-[100%] h-[200px] relative overflow-hidden flex justify-center items-center border">
+                    {item.firstimage ? (
+                      <>
+                        <img
+                          src={item.firstimage}
+                          className="max-w-[200%] sm:max-w-[100%] bsolute "
+                        />
+                      </>
+                    ) : (
+                      "noimage"
+                    )}
+                  </div>
+                  <h3 className="py-3">{item.title}</h3>
+                </li>
+              </>
+            );
+          })}
         </ul>
       </div>
     </>
